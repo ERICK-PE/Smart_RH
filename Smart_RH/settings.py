6d033667e7dcbe4bb75ed5ffdb853418355c9066
ERICK-PE <nerdgens404@gmail.com>
2026-04-13 21:11:37 -0300

    configs do django

--- Smart_RH/settings.py
+++ Smart_RH/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'Smart_RH.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'smart_rh',
         'USER': 'postgres',
         'PASSWORD': 'password',
@@ -106,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
